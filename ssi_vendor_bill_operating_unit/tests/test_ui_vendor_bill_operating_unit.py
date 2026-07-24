779b41877434d43be62786b65eaeee808d6dc438
Andhitia Rama <andhitia.r@gmail.com>
2026-07-24 22:54:51 +0700

    [UPD] ssi_vendor_bill_operating_unit
    * Perbaiki HttpCase menjadi HttpSavepointCase pada test tour Operating
      Unit -- di 14.0, HttpCase (TransactionCase polos) tidak menyediakan
      cls.env pada setUpClass, sehingga setup Pre-Condition gagal dengan
      AttributeError. HttpSavepointCase menyediakan cls.env di level class.

--- ssi_vendor_bill_operating_unit/tests/test_ui_vendor_bill_operating_unit.py
+++ ssi_vendor_bill_operating_unit/tests/test_ui_vendor_bill_operating_unit.py
@@ -2,11 +2,11 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import HttpCase, tagged
+from odoo.tests import HttpSavepointCase, tagged
 
 
 @tagged("post_install", "-at_install")
-class TestUiVendorBillOperatingUnit(HttpCase):
+class TestUiVendorBillOperatingUnit(HttpSavepointCase):
     """UI/UX tour test for the Operating Unit field this module adds to
     ``vendor_bill`` (E1 delta -- Additional Fields).
 
